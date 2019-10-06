5d0270a70fd23a6674ffa5531b4c46eb838c9de9
Pavel Nasavets <palsonbub@gmail.com>
2019-10-06 21:57:06 +0300

    Init commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "The %s service is working!", r.URL.Path[1:])
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
